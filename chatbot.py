9ab311341f797499a34b7945b6c1e742cc95e621
Ayush Bhosale <127243663+AyushBhosale@users.noreply.github.com>
2025-09-10 10:52:49 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,15 +13,13 @@ st.set_page_config(
 
 try:
     secrets = dotenv_values(".env") 
-    # GROQ_API_KEY = secrets["GROQ_API_KEY"]
-    GROQ_API_KEY='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'
+    GROQ_API_KEY = secrets["GROQ_API_KEY"]
     INITIAL_RESPONSE = secrets["INITIAL_RESPONSE"]
     INITIAL_MSG = secrets["INITIAL_MSG"]
     CHAT_CONTEXT = secrets["CHAT_CONTEXT"]
 except:
 
-    # GROQ_API_KEY=os.getenv("GROQ_API_KEY")
-    GROQ_API_KEY='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'
+    GROQ_API_KEY=os.getenv("GROQ_API_KEY")
     INITIAL_RESPONSE = os.getenv("INITIAL_RESPONSE")
     INITIAL_MSG = os.getenv("INITIAL_MSG")
     CHAT_CONTEXT = os.getenv("CHAT_CONTEXT")
